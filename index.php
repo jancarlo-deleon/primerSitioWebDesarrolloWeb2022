d38f86259236d582feb24127ab7b50a0125aed6f
Jan Carlo De León <janleoncif11@gmail.com>
2022-07-28 13:00:01 -0600

    Primer Commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PRIMER SITIO WEB JAN CARLO DE LEON</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <!--Encabezado-->
+    <header>
+        <h1>
+            Mi Hoja de Vida
+        </h1>
+    </header>
+
+    <!-- Barra de Navegacion> -->
+    <div id="menu">
+        <nav>
+            <ul>
+                <li> <a href="index.html">Bienvenida</a></li>
+                <li> <a href="datospersonales.html">Datos Personales</a></li>
+                <li> <a href="experiencialaboral.html">Experiencia Laboral</a></li>
+            </ul>
+        </nav>
+
+    </div>
+
+    <!--Imagen-->
+    <div id="imagen">
+
+        <img src="curriculum.png" alt="Imagen de un Curriculum">
+
+    </div>
+
+    <!--Footer-->
+
+    <footer>
+        <p>
+            Todos los derechos reservados a: Jan Carlo de León Cifuentes
+        </p>
+    </footer>
+   
+
+</body>
+</html>
